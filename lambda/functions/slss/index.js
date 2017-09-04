dd69ff4fc3cd16e5575145f9b8e6c0d115a80655
David Cai <davidcai1993@yahoo.com>
2017-09-04 18:14:50 +0800

    fix: fix exexFile arguments

--- lambda/functions/slss/index.js
+++ lambda/functions/slss/index.js
@@ -4,18 +4,14 @@ const { execFile } = require('child_process')
 const MAX_DELAY = 2147483647
 const EVENT_REQUIRED_KEYS = ['port', 'method', 'password']
 
-exports.slss = function (event, context, callback) {
+exports.handle = function (event, context, callback) {
   if (validateEvent(event)) printEvent(event)
   else return callback(new Error(`Invalid event: ${JSON.stringify(event)}`))
 
   // Keep event loop rolling
   setTimeout(noop, MAX_DELAY)
 
-  const ssOptions = [
-    `-k ${event.password}`,
-    `-m ${event.method}`,
-    `-p ${event.port}`
-  ]
+  const ssOptions = ['-k', event.password, '-m', event.method, '-p', event.port]
 
   execFile('./bin/shadowsocks_server', ssOptions, function (err, stdout, stderr) {
     if (err) return callback(err)
