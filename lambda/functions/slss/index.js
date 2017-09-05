fa9e04bdb4968eef0e12c92407fc91cf96286bc6
David Cai <davidcai1993@yahoo.com>
2017-09-05 10:12:34 +0800

    refactor: use "spawn" to start ss_server

--- lambda/functions/slss/index.js
+++ lambda/functions/slss/index.js
@@ -1,11 +1,12 @@
 'use strict'
-const { execFile } = require('child_process')
+const os = require('os')
+const { spawn } = require('child_process')
 
 const MAX_DELAY = 2147483647
 const EVENT_REQUIRED_KEYS = ['port', 'method', 'password']
 
 exports.handle = function (event, context, callback) {
-  if (validateEvent(event)) printEvent(event)
+  if (validateEvent(event)) print('event', event)
   else return callback(new Error(`Invalid event: ${JSON.stringify(event)}`))
 
   // Keep event loop rolling
@@ -13,10 +14,19 @@ exports.handle = function (event, context, callback) {
 
   const ssOptions = ['-k', event.password, '-m', event.method, '-p', event.port]
 
-  execFile('./bin/shadowsocks_server', ssOptions, function (err, stdout, stderr) {
-    if (err) return callback(err)
+  const server = spawn('./bin/shadowsocks_server', ssOptions)
 
-    callback(null, event)
+  server.stdout.on('data', function (data) {
+    print('ss_server stdout', data.toString())
+    callback(null, { networkInterfaces: os.networkInterfaces() })
+  })
+
+  server.stderr.on('data', function (data) {
+    print('ss_server stderr', data.toString())
+  })
+
+  server.on('close', function (code) {
+    callback(new Error(`ss_server close, code: ${code}`))
   })
 }
 
@@ -25,10 +35,10 @@ function validateEvent (event) {
   return true
 }
 
-function printEvent (event) {
-  console.log('--------------- event ---------------')
+function print (name, event) {
+  console.log(`--------------- ${name} ---------------`)
   console.log(JSON.stringify(event))
-  console.log('-------------------------------------')
+  console.log('---------------------------------------')
 }
 
 function noop () {}
